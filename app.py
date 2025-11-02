622781056fda2471e4eadfe3a4a370ad815a73f5
Francis Bacon <francisbacon@Franciss-MacBook-Air.local>
2025-11-02 16:55:27 +0000

    Change DB URL

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 gemini_client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
 
 #Change depending on whether deploying on Replit or Railway
-#MyDatabaseURL=${{ Postgres.DATABASE_URL }}
-MyDatabaseURL=os.environ['DATABASE_URL']
+MyDatabaseURL=${{ Postgres.DATABASE_URL }}
+#MyDatabaseURL=os.environ['DATABASE_URL']
 
 def get_db_connection():
     conn = psycopg2.connect(MyDatabaseURL)
